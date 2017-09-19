c871b988b86c7a0c51e81c292f058aee950ee88d
Keitaro92 <nicolas.dalmolin@gmail.com>
2017-09-19 23:35:46 +0200

    remove status bar on android

--- GemSwipe/GemSwipe.Android/MainActivity.cs
+++ GemSwipe/GemSwipe.Android/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
+using Xamarin.Forms.Platform.Android;
 
 namespace GemSwipe.Droid
 {
@@ -9,6 +11,16 @@ namespace GemSwipe.Droid
     {
         protected override void OnCreate(Bundle bundle)
         {
+            this.Window.AddFlags(WindowManagerFlags.Fullscreen | WindowManagerFlags.TurnScreenOn);
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                var stBarHeight = typeof(FormsAppCompatActivity).GetField("statusBarHeight", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+                if (stBarHeight == null)
+                {
+                    stBarHeight = typeof(FormsAppCompatActivity).GetField("_statusBarHeight", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+                }
+                stBarHeight?.SetValue(this, 0);
+            }
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
