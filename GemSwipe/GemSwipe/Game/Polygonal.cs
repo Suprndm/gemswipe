809f44d5e1e77c0cb16c8dc90876a2c003fa9c31
keitaro92 <nicolas.dalmolin@gmail.com>
2017-10-08 23:02:58 +0200

    add polygonal ( draft)

---
+++ GemSwipe/GemSwipe/Game/Polygonal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+
+namespace GemSwipe.Game
+{
+    public static class Polygonal
+    {
+        //public static IList<SKPoint> GetStarPolygon(float innerRadius, float outerRadius, int pointsCount, float angle)
+        //{
+        //    var origin = new SKPoint(0, 0);
+        //    var innerPoints = new List<SKPoint>();
+        //    for (int i = 0; i < pointsCount; i++)
+        //    {
+        //        var point = new SKPoint(
+        //            (float)Math.Cos(i * Math.PI * 2 / pointsCount)* innerRadius,
+        //            (float)Math.Sin(i * Math.PI * 2 / pointsCount)* innerRadius);
+
+
+        //        innerPoints.Add(point);
+        //    }
+        //}
+    }
+}
