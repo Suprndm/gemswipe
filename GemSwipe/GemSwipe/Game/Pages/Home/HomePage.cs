1070d51343b6c231f3437f2ae6ab29a90cb1bab9
Keitaro92 <nicolas.dalmolin@gmail.com>
2018-02-05 23:29:55 +0100

    Home page design

--- GemSwipe/GemSwipe/Game/Pages/Home/HomePage.cs
+++ GemSwipe/GemSwipe/Game/Pages/Home/HomePage.cs
@@ -1,11 +1,19 @@
 ﻿using GemSwipe.Game.Effects.BackgroundEffects;
+using GemSwipe.Game.Sprites;
+using GemSwipe.Paladin.Behaviors;
 using GemSwipe.Paladin.Navigation;
 using GemSwipe.Paladin.Navigation.Pages;
+using GemSwipe.Paladin.Sprites;
+using SkiaSharp;
 
 namespace GemSwipe.Game.Pages.Home
 {
-    public class HomePage: PageBase
+    public class HomePage : PageBase
     {
+        private Sprite _logoSprite;
+        private Sprite _bigPlanetSprite;
+        private Sprite _mediumPlanetSprite;
+        private Sprite _smallPlanetSprite;
         public HomePage()
         {
             Type = PageType.Home;
@@ -13,6 +21,33 @@ namespace GemSwipe.Game.Pages.Home
             var tapToPlay = new TapToPlay();
             AddChild(tapToPlay);
             tapToPlay.Down += OnTapped;
+
+            _logoSprite = new Sprite(SpriteConst.BelowTheStars, Width / 2, Height * 0.2f, Width * 0.33f, Height * 0.2f, new SKPaint { Color = new SKColor(255, 255, 255), BlendMode = SKBlendMode.Plus });
+            _logoSprite.Opacity = 0.7f;
+            _logoSprite.AddBehavior(new ToastBehavior(0, 0.3f, 1, 5000, 5000));
+
+            _smallPlanetSprite = new Sprite(SpriteConst.SmallPlanet, Width /2, Height * 0.7f, Width * 0.37f, Width * 0.37f, new SKPaint { Color = new SKColor(255, 255, 255) , BlendMode = SKBlendMode.Plus });
+            _smallPlanetSprite.Opacity = 0.7f;
+            _smallPlanetSprite.AddBehavior(new ToastBehavior(0, 0.2f, 1, 10000, 15000));
+
+            _mediumPlanetSprite = new Sprite(SpriteConst.MediumPlanet, Width / 2, Height * 0.5f, Width * 0.55f, Width * 0.5f, new SKPaint { Color = new SKColor(255, 255, 255), BlendMode = SKBlendMode.Plus });
+            _mediumPlanetSprite.Opacity = 0.2f;
+            _mediumPlanetSprite.AddBehavior(new ToastBehavior(0, 0, 1, 0, 12000));
+
+            _bigPlanetSprite = new Sprite(SpriteConst.MediumPlanet, Width / 2, Height * 0.2f, Width * 0.78f, Width * 0.78f, new SKPaint { Color = new SKColor(255, 255, 255), BlendMode = SKBlendMode.Plus });
+            _bigPlanetSprite.Opacity = 0.1f;
+            _bigPlanetSprite.AddBehavior(new ToastBehavior(0, -0.3f, 1, 10000, 4000));
+
+            AddChild(_smallPlanetSprite);
+
+
+            AddChild(_mediumPlanetSprite);
+
+
+            AddChild(_bigPlanetSprite);
+
+
+            AddChild(_logoSprite);
         }
 
         private void OnTapped()
@@ -22,7 +57,10 @@ namespace GemSwipe.Game.Pages.Home
 
         protected override void Draw()
         {
-         
+            float rotationSpeed = 0.001f;
+            _smallPlanetSprite.Angle += rotationSpeed;
+            _mediumPlanetSprite.Angle += rotationSpeed;
+            _bigPlanetSprite.Angle += rotationSpeed;
         }
 
         protected override void OnActivated(object parameter = null)
---
+++ GemSwipe/GemSwipe/Paladin/Behaviors/BehaviorBase.cs
@@ -0,0 +1,50 @@
+﻿using GemSwipe.Paladin.Core;
+using Xamarin.Forms;
+
+namespace GemSwipe.Paladin.Behaviors
+{
+    public abstract class BehaviorBase : IBehavior, IAnimatable
+    {
+        protected SkiaView View { get; private set; }
+
+        private bool _isDisposed;
+
+        public bool IsDisposed()
+        {
+            return _isDisposed;
+        }
+
+        public virtual void Attach(SkiaView skiaView)
+        {
+            View = skiaView;
+        }
+
+        public virtual void Detach()
+        {
+            View = null;
+        }
+
+        public void Update()
+        {
+            if (View == null || IsDisposed())
+                return;
+
+            OnUpdated();
+        }
+
+        public abstract void OnUpdated();
+
+        public void BatchBegin()
+        {
+        }
+
+        public void BatchCommit()
+        {
+        }
+
+        public virtual void Dispose()
+        {
+            _isDisposed = true;
+        }
+    }
+}
